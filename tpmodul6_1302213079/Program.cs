1b6be6f405878592676d8a6a3bc323a13d9cae14
medavidrahmadana <me.davidrahmadana@gmail.com>
2023-03-26 08:53:08 +0700

    Menambahkan class SayaTubeVideo

--- tpmodul6_1302213079/Program.cs
+++ tpmodul6_1302213079/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        this.id = new Random().Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID: {0}", this.id);
+        Console.WriteLine("Title: {0}", this.title);
+        Console.WriteLine("Play count: {0}", this.playCount);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - DAVID GHOLI RAHMADANA");
+        video.IncreasePlayCount(10);
+        video.PrintVideoDetails();
+    }
+}
