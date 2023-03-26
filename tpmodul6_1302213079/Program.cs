ea13ddf004ea21081e3bf10edae9fdd1c2c020d4
medavidrahmadana <me.davidrahmadana@gmail.com>
2023-03-26 09:29:07 +0700

    Implementasi design by contract

--- tpmodul6_1302213079/Program.cs
+++ tpmodul6_1302213079/Program.cs
@@ -8,6 +8,9 @@ class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        if (title == null || title.Length > 100)
+            throw new ArgumentException("Judul video harus memiliki panjang maksimal 100 karakter dan tidak boleh null.");
+
         this.id = new Random().Next(10000, 99999);
         this.title = title;
         this.playCount = 0;
@@ -15,11 +18,25 @@ class SayaTubeVideo
 
     public void IncreasePlayCount(int count)
     {
-        this.playCount += count;
+        if (count > 10000000)
+            throw new ArgumentException("Input penambahan play count maksimal 10.000.000 per panggilan method-nya.");
+
+        try
+        {
+            checked
+            {
+                this.playCount += count;
+            }
+        }
+        catch (OverflowException ex)
+        {
+            throw new OverflowException("Jumlah penambahan play count melebihi batas tertinggi integer.");
+        }
     }
 
     public void PrintVideoDetails()
     {
+        Console.WriteLine("=====| Messege diatas akan muncul jika Prekondisi dan Exception tidak sesuai ketentuan |=====");
         Console.WriteLine("Video ID: {0}", this.id);
         Console.WriteLine("Title: {0}", this.title);
         Console.WriteLine("Play count: {0}", this.playCount);
@@ -30,8 +47,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - DAVID GHOLI RAHMADANA");
-        video.IncreasePlayCount(10);
-        video.PrintVideoDetails();
+        // Prekondisi
+        try
+        {
+            SayaTubeVideo video1 = new SayaTubeVideo(null);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        try
+        {
+            SayaTubeVideo video2 = new SayaTubeVideo("Tutorial Design By Contract – DAVID");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        // Exception
+        SayaTubeVideo video3 = new SayaTubeVideo("Tutorial Design By Contract – DAVID GHOLI RAHMADANA");
+        try
+        {
+            video3.IncreasePlayCount(100000000);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            try
+            {
+                video3.IncreasePlayCount(1000000);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+                break;
+            }
+        }
+
+        video3.PrintVideoDetails();
     }
 }
